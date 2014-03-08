e2905ca2d9dd9a063be16dfbfd119200d83b3a57
iKurniawan <ikurniawan.fedora@gmail.com>
2014-03-08 20:32:37 +0700

    Initial commit

---
+++ CEFS/README.md
@@ -0,0 +1,10 @@
+CEFS: CentOS Errata for Spacewalk
+=================================
+
+CentOS Errata for Spacewalk based on Steve Meier errata import tool - http://cefs.steve-meier.de
+This fine piece of software allows you to import errata information from the CentOS-Announce mailing list into your Spacewalk-Server.
+For security announcements details from Red Hat (RHSA) can be imported optionally.
+
+I just made this bash shell script easier to use for me, please read the code before you use it blindly.
+
+-idk-
---
+++ CEFS/cefs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+## Configuration start ##
+MAILTO=root
+CHANNELS="centos6-x86_64 centos6-x86_64-updates centos6-x86_64-addons centos6-x86_64-contrib centos6-x86_64-extras centos6-x86_64-centosplus epel6-centos6-x86_64"
+SWSERVER="localhost"
+LOCKFILE="/var/run/spacewalk_sync.lock"
+## Configuration end ##
+
+## Spacewalk Auth = spacewalk-auth.conf file
+. ./spacewalk-auth.conf
+
+## Repository sync
+reposync()
+    {
+        if [ -e "$LOCKFILE" ]; then
+            echo "[!] Another instance already running. Aborting."
+            exit 1
+            else
+                touch "$LOCKFILE"
+        fi
+        trap "rm ${LOCKFILE}" EXIT
+
+	    for chanlabel in $CHANNELS;
+        do spacewalk-repo-sync -c $chanlabel;
+        done
+    }
+
+## Download errata file and checksums
+erratasync()
+    {
+        wget -N http://cefs.steve-meier.de/errata.latest.xml 1>/dev/null 2>&1
+        wget -N http://cefs.steve-meier.de/errata.latest.md5 1>/dev/null 2>&1
+        wget -N http://www.redhat.com/security/data/oval/com.redhat.rhsa-all.xml.bz2 1>/dev/null 2>&1
+        bunzip2 -f com.redhat.rhsa-all.xml.bz2
+        ## Integrity
+        grep "errata.latest.xml$" errata.latest.md5 > myerrata.md5
+        md5sum -c myerrata.md5 1>/dev/null 2>&1
+        if [ "$?" == 0 ]; then
+            ## ok - import errata
+            ## . ./spacewalk-auth.conf
+            ./errata-import.pl --server $SWSERVER --errata errata.latest.xml \
+            --rhsa-oval=com.redhat.rhsa-all.xml --publish 1>/dev/null
+            if [ "$?" != 0 ]; then
+                echo "[?] It seems like there was a problem while publishing the most recent errata..."
+                exit 1
+            fi
+            rm myerrata.md5
+            else
+                ## Errata information possibly invalid
+                echo "[!] ERROR: md5 checksum mismatch, check download!"
+                exit 1
+        fi
+    }
+
+reposync
+erratasync
